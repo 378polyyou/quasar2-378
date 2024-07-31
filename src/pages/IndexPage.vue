a93ccaa7bdb06f08f89ee7d869ced5cb9d87ace1
378polyyou <6604101378.poly.you@gmail.com>
2024-07-31 11:15:39 +0700

    modifile reset() method

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -110,6 +110,8 @@ export default {
       },
 
       onReset () {
+        id.value = null
+        surname.value= null
         name.value = null
         age.value = null
         accept.value = false
